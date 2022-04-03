5d79e1ccc87e42a58754eda9a886fad6b56f2afd
ITSTIME <itstime0809@gmail.com>
2022-04-03 16:44:41 +0900

    Update : SplashScreen & image add :)

--- stream_chat_ui_design/lib/main.dart
+++ stream_chat_ui_design/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:stream_chat_ui_design/component/theme.dart';
 import 'package:stream_chat_ui_design/home_screen.dart';
@@ -15,7 +17,35 @@ class MyApp extends StatelessWidget {
       theme: appTheme.light,
       darkTheme: appTheme.dark,
       themeMode: ThemeMode.dark,
-      home: const HomeScreen(),
+      home: const StreamSplashScreen(),
+    );
+  }
+}
+
+class StreamSplashScreen extends StatefulWidget {
+  const StreamSplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StreamSplashScreen> createState() => _StreamSplashScreenState();
+}
+
+class _StreamSplashScreenState extends State<StreamSplashScreen> {
+  @override
+  void initState() {
+    Timer(const Duration(milliseconds: 1500), () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => const HomeScreen()));
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset('lib/images/stream.png'),
+      ),
     );
   }
 }
